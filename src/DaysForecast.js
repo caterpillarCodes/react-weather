7eeb95cad0979172a565a6b42a22ab8bb3dc3eab
caterpillar <stm7798@gmail.com>
2024-05-18 21:19:34 +0330

    added icon

--- src/DaysForecast.js
+++ src/DaysForecast.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./DaysForecast.css";
-
+import ReactAnimatedWeather from "react-animated-weather";
 export default function DaysForecast() {
   return (
     <div className="DaysForecast">
       <h5>MON</h5>
-      <i class="fa-solid fa-cloud "></i>
+      <ReactAnimatedWeather
+        icon="CLEAR_DAY"
+        color="white"
+        size={40}
+        animate={true}
+      />
       <p class="next-day-situation">few clouds</p>
       <span className="temperature-max">15º</span>
       <span className="temperature-min">4º</span>
